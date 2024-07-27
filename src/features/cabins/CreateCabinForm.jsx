8dbaaa32e79b2ca8c8842f3fffb3ea841057e447
a-m-i-t-a-v-a <140612284+a-m-i-t-a-v-a@users.noreply.github.com>
2024-07-28 02:52:44 +0530

    Adding the error handling and refactoring

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
-
 import Input from "../../ui/Input";
 import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+import FormRow from "../../ui/FormRow";
 
-const FormRow = styled.div`
+const FormRow2 = styled.div`
   display: grid;
   align-items: center;
   grid-template-columns: 24rem 1fr 1.2fr;
@@ -33,54 +37,106 @@ const FormRow = styled.div`
   }
 `;
 
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
-
 function CreateCabinForm() {
+  const {register,handleSubmit,reset,getValues,formState}=useForm()
+  const {errors}=formState;
+  console.log(errors)
+
+  const queryClient=useQueryClient()
+
+  const {mutate,isLoading:isCreating}=useMutation({
+    mutationFn:createCabin,
+    onSuccess:()=>{
+      toast.success('New cabin successfully created');
+      queryClient.invalidateQueries({queryKey:['cabins']});
+      reset()
+    },
+    onError:(err)=>toast.error(err.message)
+  })
+
+  function onSubmit(data){
+    mutate(data)
+  }
+
+  function onError(errors){
+    console.log(errors)
+  }
+
   return (
-    <Form>
-      <FormRow>
-        <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" />
+    <Form onSubmit={handleSubmit(onSubmit,onError)}>
+      <FormRow label='Cabin Name' error={errors?.name?.message}>
+        <Input 
+          type="text" 
+          id="name" 
+          disabled={isCreating}
+          {...register('name',{
+              required:"This field is required"
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Maximum capacity' error={errors?.maxCapacity?.message}>
+        <Input 
+          type="number" 
+          id="maxCapacity" 
+          disabled={isCreating}
+          {...register('maxCapacity',{
+            required:"This field is required",
+            min:{
+              value:1,
+              message:'Capacity should be at least 1'
+            }
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Regular price' error={errors?.regularPrice?.message}>
+        <Input 
+          type="number" 
+          id="regularPrice" 
+          disabled={isCreating}
+          {...register('regularPrice',{
+            required:"This field is required",
+            validate:(value)=>value>100
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Discount' error={errors?.discount?.message}>
+        <Input 
+          type="number" 
+          id="discount" 
+          defaultValue={0} 
+          disabled={isCreating}
+          {...register('discount',{
+            required:'This field is required',
+            validate:(value)=>
+              value<getValues().regularPrice || 'Discount should be less than regular price for the sake of sanity'
+          })}/>
+      </FormRow>
+
+      <FormRow label='Description for website' error={errors?.description?.message}>
+        <Textarea 
+          type="number" 
+          id="description" 
+          defaultValue="" 
+          disabled={isCreating}
+          {...register('description',{
+            required:'This field is required'
+          })}/>
+      </FormRow>
+
+      <FormRow label='Cabin photo' error={errors?.image?.message}>
+        <FileInput id="image" accept="image/*" {...register('image',{
+            required:'This field is required'
+          })}/>
       </FormRow>
 
       <FormRow>
-        <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
-      </FormRow>
-
-      <FormRow>
-        {/* type is an HTML attribute! */}
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button>Edit cabin</Button>
+        <Button disabled={isCreating}>Add cabin</Button>
       </FormRow>
     </Form>
   );
--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 const Input=styled.input`
   border:1px solid var(--color-grey-300);
-  background-color: var(--color-grey-700);
+  background-color: var(--color-grey-100);
   border-radius:var(--border-radius-lg);
   padding:0.8rem 1.2rem;
-  box-shadow:var(--shadow-lg)
+  box-shadow:var(--shadow-sm)
 `
 
 export default Input
